6c2d890ad13d0abce8e46be260ebc7b893018ce6
xaf13 <xavierayden13@gmail.com>
2026-06-24 15:46:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
 
+search.addEventListener("input", () => {
+  const value = search.value.toLowerCase();
+
+  cards.forEach(card => {
+    const title = card.textContent.toLowerCase();
+    card.style.display = title.includes(value) ? "block" : "none";
+  });
+});
